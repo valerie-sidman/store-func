b9d0eb20f8397a4fb3f9a5c6431f8fdbab8dd82c
Valerie Sidman <valerie.sidman@gmail.com>
2021-02-08 13:25:40 +0300

    first commit

---
+++ src/models/ItemModel.js
@@ -0,0 +1,11 @@
+class ItemModel {
+  constructor (brand, title, description, descriptionFull, price) {
+    this.brand = brand;
+    this.title = title;
+    this.description = description;
+    this.descriptionFull = descriptionFull;
+    this.price = price;
+  }
+}
+
+export default ItemModel;
